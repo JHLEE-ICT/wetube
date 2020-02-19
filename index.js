c81871e7f14ea8569f393eccc1819262d497ec42
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-19 17:52:09 +0900

    #2.3
    Your First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi!");
+//require : node_modules에서 찾는 것(import)
+const express = require("express");
+//express 실행
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+//PORT에 listen을 하고 listen을 하기 시작하면 handleListeneing 함수가 실행됨
+app.listen(PORT, handleListening);
