99146c44bb5fe7e9795f8d7d5ba0777ed512ca5d
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-20 12:01:29 +0900

    #2.5 ES6 on NodeJs using Babel

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # wetube
- Cloning Youtube with Vanilla and NodeJS
+
+Cloning Youtube with Vanilla and NodeJS
+
+## dependency란?
+
+프로젝트가 실행되기위해서 필요한 것
+
+서버생성-라우터생성-그에 대한 응답순으로 진행
+
+'''npm install nodemon -D'''
+"-D"란 프로젝트를 실행할 때에 필요하지 않고 프로그래머에게 필요한 것
+즉 dependency에 포함하지 않고 설치할 때 사용
+
+nomdemon은 파일을 수정하고 일일히 npm start를 입력해줄 필요 없이 서버 실행 후 파일 수정 저장하면 자동으로 서버를 재실행 하는 것
+
+## Babel이란?
+
+최신의 JavaScript를 예전의 JavaScript로 바꿔줌으로써 표준의 코드를 얻을 수 있다.
+
+'''npm install @babel/node'''
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 //require : node_modules에서 찾는 것(import)
-const express = require("express");
+//const express = require("express");
+import express from "express";
 //express 실행
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
+const handleHome = (req, res) =>
   //응답하려면 res.send
-  //웹싸이트처럼 동작하려면 html, css파일ㅇ르 send해줘야한다
+  //웹싸이트처럼 동작하려면 html, css파일을 send해줘야한다
   res.send("Hello from home");
-}
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+//arrow function
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 
