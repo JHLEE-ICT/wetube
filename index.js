855e62106ee66a63df2e9626fdd9dfb1ac267e2c
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-20 11:43:00 +0900

    #2.4 Handling Routes with Express
    서버생성-라우터생성-그에대한응답 순으로

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  //응답하려면 res.send
+  //웹싸이트처럼 동작하려면 html, css파일ㅇ르 send해줘야한다
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 //PORT에 listen을 하고 listen을 하기 시작하면 handleListeneing 함수가 실행됨
 app.listen(PORT, handleListening);
