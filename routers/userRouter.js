1df98f129460db8a23d115d169acd21695f42711
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-21 19:51:08 +0900

    #2.11 MVC Pattern part Three

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,18 @@
 //라우터는 많은 라우트들이 담긴 파일
 import express from "express";
+import routes from "../routes";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword
+} from "../controllers/userController";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,20 @@
 import express from "express";
+import routes from "../routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  editVideo,
+  deleteVideo
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 //default : 파일로 export 한것 아닌 것은 변수로 export 한 것
 export default videoRouter;
