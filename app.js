02895008869b187b5f828626dc306caa71ee1637
JHLEE-ICT <jhlee.ict@gmail.com>
2020-02-24 10:34:49 +0900

    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//import할 때 항상 알파벳 순으로 하는 것이 좋다
 //require : node_modules에서 찾는 것(import)
 //const express = require("express");
 import express from "express";
@@ -9,10 +10,12 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 //express 실행
 const app = express();
 
+app.use(helmet());
 //view engine의 확장자를 정하는 과정, 기본폴더 => views가 되야함
 app.set("view engine", "pug");
 //middleware란 요청과 응답사이에 있는 것
@@ -20,8 +23,10 @@ app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+//local 변수를 global 변수로 사용하도록 만들어 주는 것
+app.use(localMiddleware);
 
 //use의 의미 : 누군가 /user 경로에 접속하면 이 Router 전체를 사용하겠다는 의미
 app.use(routes.home, globalRouter);
---
+++ middlewares.js
@@ -0,0 +1,9 @@
+import routes from "./routes";
+
+export const localMiddleware = (req, res, next) => {
+  //locals가 추가되면 그것들을 템플릿, 컨트롤러, 어디서든 쓸 수 있다
+  //locals에 있는 건 템플릿에 변수명처럼 존재
+  res.locals.siteName = "HyunTube";
+  res.locals.routes = routes;
+  next();
+};
