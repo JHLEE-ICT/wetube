ad7e003e5bf7ec002076b2fab07d8da69cb8b3ef
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-06 19:12:17 +0900

    #3.12 Searching Videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -15,12 +15,20 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
-export const search = (req, res) => {
+export const search = async (req, res) => {
   //searchingBy = req.query.term을 가져오는 것과 같은 방법
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const getUpload = (req, res) =>
