8e60eb248e115b02079c9ec43f7ddf4ad285daa8
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-12 01:48:47 +0900

    #6.0- #6.1 Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,14 +1,15 @@
 //init.js에 application을 호출하고 싶을 때
 
 //default로 import 했을 때
+import dotenv from "dotenv";
 import app from "./app";
 //db와의 연결
 import "./db";
-import dotenv from "dotenv";
 dotenv.config();
 //연결을 하고 model이 있다는 것을 인지하지 못하고 있기 때문에 아래 문장을 추가
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
