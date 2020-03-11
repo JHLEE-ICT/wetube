faaf1b6a855ea3ff9a3e4567ccda3b84592f874c
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-12 02:01:55 +0900

    #6.2 Local Authentication with Passport part Two

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -15,6 +16,15 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
   } else {
     // To Do : Register User
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     // To Do : Log User in
     res.redirect(routes.home);
   }
--- passport.js
+++ passport.js
@@ -2,3 +2,8 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+//serialize : 쿠키에 어떤 정보를 담아서 보낼 것인지
+passport.serializeUser(User.serializeUser());
+//deserialize : 쿠키에 있는 정보를 어떻게 사용자로 전환을 하는가
+passport.deserializeUser(User.deserializeUser());
