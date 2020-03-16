cb571659d224c7051a5a600e92e5b0a75f771382
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-16 13:45:06 +0900

    #6.6 Github Log In part One

--- passport.js
+++ passport.js
@@ -1,7 +1,17 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+  }),
+  githubLoginCallback
+);
 
 //serialize : 쿠키에 어떤 정보를 담아서 보낼 것인지
 passport.serializeUser(User.serializeUser());
