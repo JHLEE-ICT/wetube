16e5e8701ba2d8e59773237effb5c7ee3e50bb95
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-27 19:33:42 +0900

    #6.12 Facebook Log In part Three

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,13 +75,28 @@ export const postGithubLogin = (req, res) => {
 
 export const facebookLogin = passport.authenticate("facebook");
 
-export const facebookLoginCallback = (
-  accessToken,
-  refreshToken,
-  profile,
-  cb
-) => {
-  console.log(accessToken, refreshToken, profile, cb);
+export const facebookLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, name, email }
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.facebookId = id;
+      user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      facebookId: id,
+      avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
 };
 
 export const postFacebookLogin = (req, res) => {
@@ -99,6 +114,7 @@ export const getMe = (req, res) => {
 };
 
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+
 export const userDetail = async (req, res) => {
   const {
     params: { id }
