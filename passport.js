46f146a5601018c49c329717c42635f8f1dc0f26
JHLEE-ICT <jhlee.ict@gmail.com>
2020-03-27 18:43:43 +0900

    #6.11 Facebook Login Part Two

--- passport.js
+++ passport.js
@@ -28,7 +28,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://b8a56cf7.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
